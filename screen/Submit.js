c6f9713e243b5fc6d134a882888ef10f72e4a456
Pegah <Prashidnia@gmail.com>
2021-09-20 23:41:44 -0400

    form text input handling

--- screen/Submit.js
+++ screen/Submit.js
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Pressable, Button, View, Text, SafeAreaView, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_API_KEY } from "@env";
-import {Picker} from '@react-native-picker/picker';
 
 const Submit = () => {
+    const [rangeName, setRangeName] = useState('');
+    const [email, setEmail] = useState('');
+    const [website, setWebsite] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
     const accessOptions = [
         {
             key: 'out',
@@ -18,46 +22,89 @@ const Submit = () => {
             key:'both',
             text: 'Both'
         }
-    ]
+    ];
+
+    const amenities = [
+        {
+            key: 'bathroom',
+            text: 'Bathrooms'
+        },
+        {
+            key: 'equipment',
+            text: 'Equipment Rental'
+        }
+    ];
+
+    const handleValueChange = () => {
+        console.log(rangeName)
+        console.log(email)
+        console.log(phone)
+        console.log(address)
+        console.log(website)
+      }
+
     return(
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Submit a Range</Text>
-        
-            <TextInput
-                style = {styles.input}
+            <TextInput 
+                name="range"
+                style={styles.input}
                 placeholder="Range Name"
+                onChangeText={val => setRangeName(val)}
             />
-           
-            <TextInput
-                style = {styles.input}
-                placeholder="Contact Email"
-            />
-         
-            <TextInput
-                style = {styles.input}
+            <TextInput 
+                name="website"
+                style={styles.input}
                 placeholder="Website"
+                onChangeText={val => setWebsite(val)}
             />
-           
-            <TextInput
-                style = {styles.input}
+            <TextInput 
+                name="email"
+                style={styles.input}
+                placeholder="Contact Email"
+                onChangeText={val => setEmail(val)}
+            />
+            <TextInput 
+                name="phone"
+                style={styles.input}
                 placeholder="Phone"
+                onChangeText={val => setPhone(val)}
             />
-            <TextInput
-                style = {styles.input}
+            <TextInput 
+                name="address"
+                style={styles.input}
                 placeholder="Address"
+                onChangeText={val => setAddress(val)}
             />
+                
             <Text style={{fontSize: 20, padding: 10}}>Access</Text>
-            {accessOptions.map(item => {
-                return(
-                    <View key={item.key} style={styles.buttonContainer}>
-                        <TouchableOpacity style={styles.circle}/>
-                        <Text style={styles.options}>
-                            {item.text}
-                        </Text>
-                    </View>);
-            })}
-            <Text style={{fontSize: 20}}>Amenities</Text>
+            <View style={{flexDirection: 'row', margin: 10}}>
+                {accessOptions.map(item => {
+                    return(
+                        <View key={item.key} style={styles.buttonContainer}>
+                            <TouchableOpacity style={styles.circle} />
+                            <Text style={styles.options}>
+                                {item.text}
+                            </Text>
+                        </View>);
+                    })
+                }
+            </View>
+            <Text style={{fontSize: 20, padding: 10}}>Amenities</Text>
+            <View style={{flexDirection: 'row', margin: 10}}>
+                {amenities.map(item => {
+                    return(
+                        <View key={item.key} style={styles.buttonContainer}>
+                            <TouchableOpacity style={styles.checkBox}/>
+                            <Text style={styles.options}>
+                                {item.text}
+                            </Text>
+                        </View>);
+                    })
+                }
+            </View>
             <Button 
+                onPress={handleValueChange}
                 title="Submit"
                 color="black"
             />
@@ -79,10 +126,10 @@ const styles = StyleSheet.create({
         fontSize: 20,
         borderWidth: 1,
         borderColor: 'gray',
-        padding: 10,
+        padding: 8,
         width: '80%',
         borderRadius: 20,
-        margin: 10
+        margin: 8
     },
     buttonContainer: {
         flexDirection: 'row',
@@ -96,6 +143,12 @@ const styles = StyleSheet.create({
         borderColor: '#ACACAC',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    checkedCircle: {
+        width: 14,
+        height: 14,
+        borderRadius: 7,
+        backgroundColor: '#794F9B',
     },
     options: {
         fontSize: 17
